ea96a016d0b45d52f8366e7a128d2e10f546692b
grovesk <keir.groves@global.amentum.com>
2025-06-12 14:43:21 +0100

    made a def for online processing

--- demo.py
+++ demo.py
@@ -278,8 +278,7 @@ def main(experiment_number):
 
 if __name__ == "__main__":
     # your experiment here:
-    experiment_number = 3
-
+    experiment_number = 7
     # sanity check
     assert 1 <= experiment_number <= 10, "experiment_number must be 1–10"
 
